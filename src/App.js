a7a9be04b442ae7993e3d00d2f9857a0e8ee37e4
Crostons <shubh1608003@gmail.com>
2023-03-22 14:17:26 +0530

    Feed and verse component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BNavbar from "./components/BNavbar";
 import Header from "./components/Header";
+import Feed from "./components/Feed";
+import Verse from "./components/Verse";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Verse />
       <BNavbar />
     </div>
   );
