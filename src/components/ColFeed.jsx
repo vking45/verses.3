32b22cfe4c27af5ca989393d4b208275ff4089c7
vking45 <d.vraj.4523@gmail.com>
2023-03-26 12:51:30 +0530

    final_commit b4 submission

--- src/components/ColFeed.jsx
+++ src/components/ColFeed.jsx
@@ -66,16 +66,15 @@ function ColFeed() {
     <div className='h-screen bg-main font-jura'>
     <div class="container px-5 py-24 mx-auto">
       <div className='inline-flex'>
-        <h1 class="sm:text-3xl text-3xl font-medium title-font text-primary mb-4 inline-flex">{ loaded ? title : "Loading..."}:
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block mt-2 ml-2 w-5 h-5">
+      <h1 class="sm:text-3xl text-3xl font-medium title-font text-primary mb-4 inline-flex mr-10">{ loaded ? title : "Loading..."}</h1>
+        <button onClick={(e) => addSub(collectionRef.record(id), profileReference.record(state.publicKey))} type="button" class= { subscribed ? "inline-flex text-primary bg-sherlock hover:bg-thor border border-thor font-jura font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2" : "inline-flex text-primary bg-thor hover:bg-loki border border-thor font-jura font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2"} disabled={subscribed}> { subscribed ? "Subscribed" : "Subscribe" }
+              <div className="mx-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-flex mx-1 w-5 h-5">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
                 </svg>
-                { pvt ? price : "" }</h1>
- 
-        <div className='ml-20'>
-        <button onClick={(e) => addSub(collectionRef.record(id), profileReference.record(state.publicKey))} type="button" class= { subscribed ? "inline-block text-primary bg-sherlock hover:bg-thor border border-thor font-jura font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2" : "inline-block text-primary bg-thor hover:bg-loki border border-thor font-jura font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2"} disabled={subscribed}> { subscribed ? "Subscribed" : "Subscribe" }
+                { pvt ? price : 0 }
+              </div>
         </button>
-        </div>
         </div>
     <div class="flex flex-wrap -m-4">
           {loaded ? 
